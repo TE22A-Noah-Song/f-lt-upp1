fa568c28b2fadccf42ae290b3c2a49752d921a8b
Noah Söng <noah.song@elev.ga.ntig.se>
2023-09-25 09:22:32 +0200

    gjort upp a)

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+int[]fält1={8,4,6,3,9};
+System.out.println(fält1);
+
+int antal=fält1.length;
+System.out.println(antal);  
+
+double summa=fält1[0]+fält1[1]+fält1[2]+fält1[3]+fält1[4];
+double medelvärde=summa/5;
+System.out.println(medelvärde);
+
+    }
+}
